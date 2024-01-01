594099838257d1494068912bdd2ace216d591226
HemantSharma909 <104320688+HemantSharma909@users.noreply.github.com>
2024-01-01 09:13:32 +0000

    new_changes

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,3 +1,21 @@
 let isloggedin=1;
 let booleanisloggedin= Boolean(isloggedin);
 console.log(booleanisloggedin);
+
+// **********OPERATIONS************* //
+
+let str1= "Hello";
+let str2= " Hemant";
+let str3= str1+str2;
+console.log(str3);
+
+//complex situations
+
+console.log(1+"2");
+console.log("1"+2);
+console.log(typeof(1+"2"));
+console.log(typeof("1"+2));
+
+console.log("1"+2+2);// 122
+console.log(1+2+"2");//32
+console.log(typeof(1+2+"2"));
